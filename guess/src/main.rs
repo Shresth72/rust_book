e28f3bf9ee059c7b05dff8ee7f4e8411166f0fa2
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2023-12-18 20:36:46 +0530

    colored library

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
+use colored::*;
 
 fn main() {
     println!("Guess the number!");
@@ -9,13 +10,32 @@ fn main() {
 
     println!("The secret number is : {}", secret);
 
-    println!("Please input your guess.");
-
-    let mut guess: String = String::new();
-
-    io::stdin()
-        .read_line(&mut guess) // append to ref to the guess string
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
+    loop {
+        println!("Please input your guess.");
+    
+        let mut guess: String = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess) // append to ref to the guess string
+            .expect("Failed to read line");
+    
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{}", "The input must be a number".red());
+                continue;
+            },
+        };
+    
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            },
+        }
+    }
 }
