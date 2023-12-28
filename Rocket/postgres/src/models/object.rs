a38c896895c152550a4db02e1d41b0ce51a1a14a
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2023-12-28 19:13:37 +0530

    aws-lambda & docker

--- Rocket/postgres/src/models/object.rs
+++ Rocket/postgres/src/models/object.rs
@@ -24,21 +24,21 @@ pub enum MyError {
 impl<'r> FromData<'r> for Object {
     type Error = MyError;
 
-    async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> Outcome<Self, (Status, MyError), Data<'r>> {
+    async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> Result<Value, (Status, MyError)> {
         // Read the data into a String.
         let string_data = match data.open(10.kilobytes()).into_string().await {
             Ok(s) => s,
-            Ok(_) => return Outcome::Failure((Status::BadRequest, MyError::TooLarge)),
-            Err(e) => return Outcome::Failure((Status::InternalServerError, MyError::Io(e))),
+            Ok(_) => return Err((Status::BadRequest, MyError::TooLarge)),
+            Err(e) => return Err((Status::InternalServerError, MyError::Io(e))),
         };
 
         let string_body = request::local_cache!(req, string_data);
 
         match serde_json::from_str(string_body) {
-            Ok(value) => Outcome::Success(value),
+            Ok(value) => Ok(value),
             Err(e) => {
                 println!("Failed to deserialize: {:?}", e);
-                Outcome::Failure((Status::BadRequest, MyError::NoBodyProvided))
+                Err((Status::BadRequest, MyError::NoBodyProvided))
             }
         }
     }
